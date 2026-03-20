3ff5ff0883a1cc5c8a62eac52614830a67dbe835
Ahmed Sayed <a.marraey@gmail.com>
2026-03-20 03:50:00 +0200

    Finish working in Interactive pricing component

--- script/script.js
+++ script/script.js
@@ -5,20 +5,19 @@ const viewsEL = document.querySelector(".header-subtitle")
 const softCyan = styles.getPropertyValue("--Soft-Cyan").trim()
 const strongCyan = styles.getPropertyValue("--Light-Grayish-Blue").trim()
 
-
 data = [
-    { views: "10K" , price: 8 },
-    { views: "50K" , price: 12 },
-    { views: "100K" , price: 16 },
-    { views: "500K" , price: 24 },
-    { views: "1M" , price: 36 },
+    { views: "10K", price: 8 },
+    { views: "50K", price: 12 },
+    { views: "100K", price: 16 },
+    { views: "500K", price: 24 },
+    { views: "1M", price: 36 },
 ]
 
 
-function updateSlider(){
+function updateSlider() {
     const value = Number(slider.value)
     const max = Number(slider.max)
-    const percent = (value/max) *100
+    const percent = (value / max) * 100
     slider.style.background = `linear-gradient( to right , ${softCyan} ${percent}% , ${strongCyan} ${percent}%)`
     viewsEL.textContent = `${data[value].views} Pageviews`
     priceEL.textContent = `$${data[value].price.toFixed(2)}`
@@ -26,6 +25,4 @@ function updateSlider(){
 
 updateSlider()
 
-slider.addEventListener("input" , updateSlider)
-
-// window.addEventListener("load", updateSlider)
+slider.addEventListener("input", updateSlider)
